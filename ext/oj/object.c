113531835da6121e69efb2e48c419a695bd704ae
Peter Ohler <peter@ohler.com>
2017-05-05 21:03:51 -0700

    debug

--- ext/oj/object.c
+++ ext/oj/object.c
@@ -301,7 +301,6 @@ hat_num(ParseInfo pi, Val parent, Val kval, NumInfo ni) {
 		    args[4] = LONG2NUM(st->tm_min);
 		    args[5] = rb_float_new((double)st->tm_sec + ((double)nsec + 0.5) / 1000000000.0);
 		    args[6] = LONG2NUM(ni->exp);
-		    
 		    parent->val = rb_funcall2(rb_cTime, oj_new_id, 7, args);
 		} else {
 		    parent->val = rb_time_nano_new(ni->i, (long)nsec);
